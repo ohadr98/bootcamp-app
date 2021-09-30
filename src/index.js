7a9964efafca14fb1f555f12da11266dcd718174
ohadr98 <83167837+ohadr98@users.noreply.github.com>
2021-09-30 23:44:22 +0300

    update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const init = async () => {
 	dotenv.config();
 	const config = {
 		port: process.env.PORT || 8080,
-		host: process.env.HOST || "34.90.223.57"
+		host: process.env.HOST || "0.0.0.0"
 	};
 	const server = await createServer( config );
 	await server.start();
