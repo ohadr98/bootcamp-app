905a8d751b077df78d3899ba2c35d12443e526fc
ohadr98 <83167837+ohadr98@users.noreply.github.com>
2021-09-30 22:53:10 +0300

    change localhost

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const init = async () => {
 	dotenv.config();
 	const config = {
 		port: process.env.PORT || 3000,
-		host: process.env.HOST || "localhost"
+		host: process.env.HOST || "34.90.223.57"
 	};
 	const server = await createServer( config );
 	await server.start();
